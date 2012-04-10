3af5bbf0af2439431ff7551691c0152365396879
Geoff Sobering <geoff@geoff-s.net>
2012-04-09 23:42:44 -0500

    Last item is using the "titleborder" css.

--- Products.f
+++ Products.f
@@ -17,6 +17,17 @@
       <p>Blackie Carbon Carbon cloth on outside of mast gloss epoxy finish competition layup.</p>
    </div>
    <div class="span-2 right product">$1400.00</div>
+   <hr />
+   <div class="titleborder" >
+   <span class="titleborder">Runner Sharpener</span>
+      <div class="span-3 push-1 product clear" style="margin:0;">
+         <img class="tn" src="products/stdMast_tn.jpg" width="100" height="100" />
+      </div>
+      <div class="span-11 product push-1">
+         <p>A benchtop runner sharpener for use with all types of runners and all sharpening angles.</p>
+      </div>
+      <div class="span-2 right product">$1400.00</div>
+   </div>
    <hr class="product" />
 </div>
 </fragment>
