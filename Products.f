6db2c376cf4ba09648476f907b732b26c3e501a0
Geoff Sobering <geoff@geoff-s.net>
2012-04-09 23:02:33 -0500

    New layout just using blueprint.

--- Products.f
+++ Products.f
@@ -1,3 +1,22 @@
 <fragment>
-<p>Products</p>
+<div class="product">
+   <div class="span-20 left product"><h1 style="margin:10px;">Products</h1></div>
+   <hr class="product" />
+   <div class="span-3 push-1 product clear">
+      <img src="products/stdMast_tn.jpg" width="100" height="100" class="product"/>
+   </div>
+   <div class="span-13 product">
+      <p class="product" >Blackie Carbon Carbon cloth on outside of mast gloss epoxy finish competition layup.</p>
+   </div>
+   <div class="span-2 right product">$1400.00</div>
+   <hr class="product" />
+   <div class="span-3 push-1 product clear">
+      <img src="products/stdMast_tn.jpg" width="100" height="100" />
+   </div>
+   <div class="span-13 product">
+      <p>Blackie Carbon Carbon cloth on outside of mast gloss epoxy finish competition layup.</p>
+   </div>
+   <div class="span-2 right product">$1400.00</div>
+   <hr class="product" />
+</div>
 </fragment>
