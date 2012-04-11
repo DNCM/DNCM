16fcecaa2950f9fa096138f55796b75d3551d872
Geoff Sobering <geoff@geoff-s.net>
2012-04-11 00:27:24 -0500

    Typos, grammer, and errors - Oh my!

--- index.f
+++ index.f
@@ -1,12 +1,13 @@
 <fragment>
 <img src="20120107DN19250-X2_790x236.jpg" width="790" height="236" />
 <p class="normal">For 14 years Bob Rast and <span class="inlineCompanyName">RWR Composites</span> have been developing composite DN masts.<br />
-The end result is their line of extremely durable, competitive DN racing masts available at a affordable price.<br />
-All <span class="inlineCompanyName">RWR Composites</span> masts are custom built for modern sail-cut and high boat-speed
-using the best quality aerospace epoxy and a mix of carbon-fiber, S-glass and E-glass. Blow-molding technology is used to assure the highest quaility 
+The end result is their line of extremely durable, competitive DN racing masts available at affordable prices.<br />
+All <span class="inlineCompanyName">RWR Composites</span> masts are custom built for the modern DN sail-cut and high boat-speed
+using the best quality aerospace epoxy and a mix of carbon-fiber, S-glass and E-glass. Blow-molding technology is used to assure the highest quality 
 laminate.
 </p>
 <p class="normal">
-For more information on <span class="inlineCompanyName">RWR Composites</span> masts and other DN hardware, please visit our <a href="Products.html">Products</a> page.
+For more information on <span class="inlineCompanyName">RWR Composites</span> masts and other DN hardware, 
+please visit our <a href="Products.html">Products</a> page or <a href="Contact.html">contact us</a> directly..
 </p>
 </fragment>
