b973201b5aa265b2bcffc23f4a648dc5d3eec9f7
Geoff Sobering <geoff@geoff-s.net>
2012-04-11 00:32:45 -0500

    More tweeks...

--- index.f
+++ index.f
@@ -1,4 +1,5 @@
 <fragment>
+<div class="home">
 <img src="20120107DN19250-X2_790x236.jpg" width="790" height="236" />
 <p class="normal">For 14 years Bob Rast and <span class="inlineCompanyName">RWR Composites</span> have been developing composite DN masts.<br />
 The end result is their line of extremely durable, competitive DN racing masts available at affordable prices.<br />
@@ -8,6 +9,7 @@ laminate.
 </p>
 <p class="normal">
 For more information on <span class="inlineCompanyName">RWR Composites</span> masts and other DN hardware, 
-please visit our <a href="Products.html">Products</a> page or <a href="Contact.html">contact us</a> directly..
+please visit our <a href="Products.html">Products</a> page or <a href="Contact.html">contact us</a> directly.
 </p>
+</div>
 </fragment>
