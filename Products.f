aeb372459d294d179605494cdea7d15f6fa4301b
Geoff Sobering <geoff@geoff-s.net>
2012-04-10 20:47:31 -0500

    Produts page done (for now...)

--- Products.f
+++ Products.f
@@ -10,10 +10,10 @@
    </div>
    <div class="span-14 product">
       <p>The <span class="productName">Blackie Carbon</span> mast is made of a combination of carbon-fiber 
-      and fiberglass laminated with Gougeon Pro-Set epoxy and post-cured in an oven.
+      and fiberglass laminated with aircraft-grade epoxy and post-cured in an oven.
       The carbon cloth is on outside of mast with a gloss epoxy finish.</p>
    </div>
-   <div class="span-2 right product">$1400.00</div>
+   <div class="span-2 right product">$1400</div>
    <hr class="product" />
    <div class="span-1 clear spacer">.</div>
    <div class="span-3 push-1 product" style="margin:0;">
@@ -23,7 +23,7 @@
       <p>The <span class="productName">Black Max</span> has the same layup and construction as the <i>Blackie</i> 
       but with the carbon-fiber on the inside of the mast. It has a black graphite finish.</p>
    </div>
-   <div class="span-2 right product">$1400.00</div>
+   <div class="span-2 right product">$1400</div>
    <hr class="product" />
    <div class="span-1 clear spacer">.</div>
    <div class="span-3 push-1 product" style="margin:0;">
@@ -31,9 +31,9 @@
    </div>
    <div class="span-14 product">
       <p>The <span class="productName">Twizzler</span> uses the same construction methods as the <i>Blackie</i> and <i>Black Max</i>
-      but it is softer for lighter sailors. It can have a dyed finish in red, blue, green, yellow, or grey</p>
+      but it is bendier for lighter sailors. It can have a dyed finish in red, blue, green, yellow, or grey</p>
    </div>
-   <div class="span-2 right product">$1400.00</div>
+   <div class="span-2 right product">$1400</div>
    <div class="span-20 clear"><hr class="sectionSeparator" /></div>
    <div class="span-20 left clear productSubHeading"><span class="productSubHeading">Other Items:</span></div>
    <hr class="product" />
@@ -42,8 +42,29 @@
       <img class="tn" src="products/runnerSharpener_tn.jpg" width="100" height="100" />
    </div>
    <div class="span-14 product">
-      <p>A benchtop runner sharpener for use with all types of runners and all sharpening angles.</p>
+      <p><span class="productName">Benchtop Runner Sharpener</span> For use with all types of runners and all sharpening angles. 
+      Constructed with carbon-fiber re-enforcement for rigidity and stability with a 1hp reversible motor.
+      The sharpener uses readily available 2x90" belts.</p>
    </div>
-   <div class="span-2 right product">$750.00</div>
+   <div class="span-2 right product">$750</div>
+   <hr class="product" />
+   <div class="span-1 clear spacer">.</div>
+   <div class="span-3 push-1 product" style="margin:0;">
+      <img class="tn" src="products/placeholder.png" width="100" height="100" />
+   </div>
+   <div class="span-14 product">
+      <p><span class="productName">Carbon fiber Runner Stiffeners</span></p>
+   </div>
+   <div class="span-2 right product">$100/pair</div>
+   <hr class="product" />
+   <div class="span-1 clear spacer">.</div>
+   <div class="span-3 push-1 product" style="margin:0;">
+      <img class="tn" src="products/placeholder.png" width="100" height="100" />
+   </div>
+   <div class="span-14 product">
+      <p><span class="productName">Carbon-Fiber Hiking Rack.</span> 12" long.</p>
+   </div>
+   <div class="span-2 right product">$30</div>
+
 </div>
 </fragment>
