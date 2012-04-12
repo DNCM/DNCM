de90517bd7f5f61612f3407e38d0c4c690450f59
Geoff Sobering <geoff@geoff-s.net>
2012-04-11 22:00:25 -0500

    Fixed RKR...

--- index.f
+++ index.f
@@ -1,14 +1,14 @@
 <fragment>
 <div class="home">
 <img src="20120107DN19250-X2_790x236.jpg" width="790" height="236" />
-<p class="normal">For 14 years Bob Rast and <span class="inlineCompanyName">RWR Composites</span> have been developing composite DN masts.<br />
+<p class="normal">For 14 years Bob Rast and <span class="inlineCompanyName">RKR Composites</span> have been developing composite DN masts.<br />
 The end result is their line of extremely durable, competitive DN racing masts available at affordable prices.<br />
-All <span class="inlineCompanyName">RWR Composites</span> masts are custom built for the modern DN sail-cut and high boat-speed
+All <span class="inlineCompanyName">RKR Composites</span> masts are custom built for the modern DN sail-cut and high boat-speed
 using the best quality aerospace epoxy and a mix of carbon-fiber, S-glass and E-glass. Blow-molding technology is used to assure the highest quality 
 laminate.
 </p>
 <p class="normal">
-For more information on <span class="inlineCompanyName">RWR Composites</span> masts and other DN hardware, 
+For more information on <span class="inlineCompanyName">RKR Composites</span> masts and other DN hardware, 
 please visit our <a href="Products.html">Products</a> page or <a href="Contact.html">contact us</a> directly.
 </p>
 </div>
