cf092bb3f28913fa562332957c8b6618ec0e009a
Geoff Sobering <geoff@geoff-s.net>
2012-04-12 22:23:11 -0500

    Typos.

--- index.f
+++ index.f
@@ -3,8 +3,8 @@
 <img src="20120107DN19250-X2_790x236.jpg" width="790" height="236" />
 <p class="normal">For 14 years Bob Rast and <span class="inlineCompanyName">RKR Composites</span> have been developing composite DN masts.<br />
 The end result is their line of extremely durable, competitive DN racing masts available at affordable prices.<br />
-All <span class="inlineCompanyName">RKR Composites</span> masts are custom built for the modern DN sail-cut and high boat-speed
-using the best quality aerospace epoxy and a mix of carbon-fiber, S-glass and E-glass. Blow-molding technology is used to assure the highest quality 
+All <span class="inlineCompanyName">RKR Composites</span> masts are custom built for the modern DN sail-cut
+using aerospace epoxy and a mix of carbon-fiber, S-glass and E-glass. Blow-molding technology is used to assure the highest quality 
 laminate.
 </p>
 <p class="normal">
