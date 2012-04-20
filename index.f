a51123ceebe42da9638e68db364315b039853ecf
Geoff Sobering <geoff@geoff-s.net>
2012-04-19 23:28:43 -0500

    Added Cathy Firmbach's name on her photo and Gretchen's URL

--- index.f
+++ index.f
@@ -2,7 +2,7 @@
 <div class="home">
 <div style="position: relative">
    <img class="photoWithCredit" src="20120107DN19250-X2_790x236.jpg" width="790" height="236" />
-   <span class="photoCredit"><a href="http://gretchendorian.com/">Photo: Gretchen Dorian</a></span>
+   <span class="photoCredit"><a href="http://gretchendorian.com/" target="_blank" >Photo: Gretchen Dorian</a></span>
 </div>
 <p class="normal">For 14 years Bob Rast and <span class="inlineCompanyName">RKR Composites</span> have been developing composite DN masts.<br />
 The end result is their line of extremely durable, competitive DN racing masts available at affordable prices.<br />
@@ -13,6 +13,9 @@ laminate.
 <p class="normal">
 For more information on <span class="inlineCompanyName">RKR Composites</span> masts and other DN hardware, 
 please visit our <a href="Products.html">Products</a> page or <a href="Contact.html">contact us</a> directly.
+</p>
+<hr />
+<p class="normal">
 </p>
 </div>
 </fragment>
