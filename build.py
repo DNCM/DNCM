828fb66645de7a65dc35ac9937345915d4253b39
unknown <GeoffS@.(none)>
2012-04-21 12:45:31 -0500

    Started changes to process photos

--- build.py
+++ build.py
@@ -2,10 +2,15 @@ from xml.etree.ElementTree import ElementTree
 import xml.etree.ElementTree as ET
 import os
 
+doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">\n'
+
 #links = [['index', 'Home'], ['Products', 'Products'], ['Photos', 'Photos'], ['About', 'About Us'], ['Contact', 'Contact Us'], ['ForSale', 'For Sale']]
 links = [['index', 'Home'], ['Products', 'Products'], ['Videos', 'Videos'], ['Contact', 'Contact Us']]
-templateFile = 'page_template.html'
-destDir = 'www'+os.sep
+photos = ['20120107DN19323-X3_650x650', 'DSC_5487 (1)a_900x600']
+pageTemplateFile = 'page_template.html'
+photoTemplateFile = 'image_template.html'
+topDestDir = 'www'+os.sep
+photoDestDir = topDestDir+os.sep+'images'+os.sep
 
 
 class CommentedTreeBuilder ( ET.XMLTreeBuilder ):
@@ -18,14 +23,14 @@ class CommentedTreeBuilder ( ET.XMLTreeBuilder ):
         self._target.data( data )
         self._target.end( ET.Comment )
 
-def findContentDiv(parentElem):
+def findDivWithId(parentElem, id):
 	for elem in list(parentElem):
 		if elem.tag == 'div':
 			idAttrib = elem.get('id')
 			if idAttrib != None:
-				if idAttrib == 'ContentDiv':
+				if idAttrib == id:
 					return elem
-		candidateElem = findContentDiv(elem)
+		candidateElem = findDivWithId(elem, id)
 		if candidateElem != None:
 			return candidateElem
 	return None
@@ -69,12 +74,13 @@ def insertSidebarLinks(templateRoot, thisFile, links):
 		
 		sidebarElem.insert(0, pElem)
 
+print "Creating pages:"
 for linkTuple in links:
 	srcFileRoot = linkTuple[0]
 	
 	srcFile = srcFileRoot+'.f'
-	destFile = destDir+srcFileRoot+'.html' 
-	print srcFile+' + '+templateFile+' => '+destFile
+	destFile = topDestDir+srcFileRoot+'.html' 
+	print "   "+srcFile+' + '+photoTemplateFile+' => '+destFile
 	
 	try:
 		os.remove(destFile)
@@ -84,14 +90,12 @@ for linkTuple in links:
 		pass
 	
 	templateTree = ElementTree()
-	templateRoot = templateTree.parse("page_template.html", parser = CommentedTreeBuilder())
-	
-	insertSidebarLinks(templateRoot, srcFileRoot, links)
+	templateRoot = templateTree.parse(pageTemplateFile, parser = CommentedTreeBuilder())
 	
 	srcTree = ElementTree()
 	srcRoot = srcTree.parse(srcFile, parser = CommentedTreeBuilder())
 	
-	contentDiv = findContentDiv(templateRoot)
+	contentDiv = findDivWithId(templateRoot, 'ContentDiv')
 	if contentDiv == None:
 		print "ERROR: No Content Div found!"
 		exit()
@@ -109,8 +113,53 @@ for linkTuple in links:
 			headElem.append(elem)
 	
 	dest = open(destFile, 'w')
-	dest.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">\n')
+	dest.write(doctype)
 	templateTree.write(dest)
 	dest.close()
+
+exit()
+print "Creating photos:"
+for photoRoot in photos:
+    
+    srcFile = photoRoot+'.xml'
+    destFile = photoRoot+srcFileRoot+'.html' 
+    print "   "+srcFile+' + '++' => '+destFile
+    
+    try:
+        os.remove(destFile)
+    except WindowsError as (errno, strerror):
+        #print "WindowsError({0}): {1}".format(errno, strerror)
+        #print "Note: No "+destFile+" to remove."
+        pass
+    
+    templateTree = ElementTree()
+    templateRoot = templateTree.parse("page_template.html", parser = CommentedTreeBuilder())
+    
+    insertSidebarLinks(templateRoot, srcFileRoot, links)
+    
+    srcTree = ElementTree()
+    srcRoot = srcTree.parse(srcFile, parser = CommentedTreeBuilder())
+    
+    contentDiv = findDivWithId(templateRoot)
+    if contentDiv == None:
+        print "ERROR: No Content Div found!"
+        exit()
+    #print "contentDiv.text='"+contentDiv.text+"'"
+    #print "contentDiv.tail='"+contentDiv.tail+"'"
+    
+    for elem in srcRoot.getchildren():
+        contentDiv.append(elem)
+    
+    if srcFileRoot == 'index':
+        metaTree = ElementTree()
+        metaRoot = metaTree.parse('meta.f', parser = CommentedTreeBuilder())
+        headElem = templateRoot.find('head')
+        for elem in metaRoot.getchildren():
+            headElem.append(elem)
+    
+    dest = open(destFile, 'w')
+    dest.write(doctype)
+    templateTree.write(dest)
+    dest.close()
 
 print "Done!"
