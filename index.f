fc5b85e69bdf0900b2d6c5c3c84fb8f65addbf9b
Geoff Sobering <geoff@geoff-s.net>
2012-04-20 21:37:54 -0500

    Added Jori's photo on the left of the main page.

--- index.f
+++ index.f
@@ -2,7 +2,7 @@
 <div class="home">
 <div style="position: relative">
    <img class="photoWithCredit" src="20120107DN19250-X2_790x236.jpg" width="790" height="236" />
-   <span class="photoCredit"><a href="http://gretchendorian.com/" target="_blank" >Photo: Gretchen Dorian</a></span>
+   <span class="photoCreditDark"><a href="http://gretchendorian.com/" target="_blank" >Photo: Gretchen Dorian</a></span>
 </div>
 <p class="normal">For 14 years Bob Rast and <span class="inlineCompanyName">RKR Composites</span> have been developing composite DN masts.<br />
 The end result is their line of extremely durable, competitive DN racing masts available at affordable prices.<br />
@@ -14,5 +14,15 @@ laminate.
 For more information on <span class="inlineCompanyName">RKR Composites</span> masts and other DN hardware, 
 please visit our <a href="Products.html">Products</a> page or <a href="Contact.html">contact us</a> directly.
 </p>
+<hr style="margin-top:10px;" />
+<div class="span-9 left colborder">
+   <img src="blueprint-placeholders-full/PNG/images/square/span-9.png" />
+</div>
+<!-- div class="span-1 colborder">c</div -->
+<div class="span-10 right last"><div style="width:350px; position: relative">
+   <a href="images/DSC_5487 (1)a_900x600.html" ><img  class="photoWithCreditLight" src="images/DSC_5487a_350x350.jpg" width="350" height="350"/></a>
+   <span class="photoCreditLight"><a href="http://cefirmbach.com/index.html" target="_blank" >Photo: Catherine E Firmbach</a></span>
+</div>
+<p style="margin-left:20px; margin-right:60px;">Jori Lenon and her bright red <i>Twizzler</i> mast round the windward mark during the 2012 DN North American Championship Regatta on Green Bay.</p></div>
 </div>
 </fragment>
