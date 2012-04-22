a13b0c33945d71c749b639910b23d6e94e28cf2d
unknown <GeoffS@.(none)>
2012-04-22 00:16:43 -0500

    Added code to create photo pages.

--- build.py
+++ build.py
@@ -22,14 +22,14 @@ class CommentedTreeBuilder ( ET.XMLTreeBuilder ):
         self._target.data( data )
         self._target.end( ET.Comment )
 
-def findDivWithId(parentElem, id):
+def findElemWithId(parentElem, tag, id):
 	for elem in list(parentElem):
-		if elem.tag == 'div':
+		if elem.tag == tag:
 			idAttrib = elem.get('id')
 			if idAttrib != None:
 				if idAttrib == id:
 					return elem
-		candidateElem = findDivWithId(elem, id)
+		candidateElem = findElemWithId(elem, tag, id)
 		if candidateElem != None:
 			return candidateElem
 	return None
@@ -97,7 +97,7 @@ for linkTuple in links:
 	srcRoot = srcTree.parse(srcFile, parser = CommentedTreeBuilder())
     
 	
-	contentDiv = findDivWithId(templateRoot, 'ContentDiv')
+	contentDiv = findElemWithId(templateRoot, 'div', 'ContentDiv')
 	if contentDiv == None:
 		print "ERROR: No Content Div found!"
 		exit()
@@ -123,45 +123,48 @@ print "Creating photos:"
 photosTree = ElementTree()
 photosRoot = templateTree.parse("photos.xml")
 for photoElem in photosRoot:
-    
     srcFileRoot = photoElem.find('name').text
     srcFile = srcFileRoot+'.jpg'
     destFile = photoDestDir+srcFileRoot+'.html'
     print "   "+srcFile+' + '+photoDestDir+' => '+destFile
     
-#    try:
-#        os.remove(destFile)
-#    except WindowsError as (errno, strerror):
-#        #print "WindowsError({0}): {1}".format(errno, strerror)
-#        #print "Note: No "+destFile+" to remove."
-#        pass
-#    
-#    templateTree = ElementTree()
-#    templateRoot = templateTree.parse("page_template.html", parser = CommentedTreeBuilder())
-#    
-#    srcTree = ElementTree()
-#    srcRoot = srcTree.parse(srcFile, parser = CommentedTreeBuilder())
-#    
-#    contentDiv = findDivWithId(templateRoot)
-#    if contentDiv == None:
-#        print "ERROR: No Content Div found!"
-#        exit()
-#    #print "contentDiv.text='"+contentDiv.text+"'"
-#    #print "contentDiv.tail='"+contentDiv.tail+"'"
-#    
-#    for elem in srcRoot.getchildren():
-#        contentDiv.append(elem)
-#    
-#    if srcFileRoot == 'index':
-#        metaTree = ElementTree()
-#        metaRoot = metaTree.parse('meta.f', parser = CommentedTreeBuilder())
-#        headElem = templateRoot.find('head')
-#        for elem in metaRoot.getchildren():
-#            headElem.append(elem)
-#    
-#    dest = open(destFile, 'w')
-#    dest.write(doctype)
-#    templateTree.write(dest)
+    height = photoElem.find('height').text
+    width = photoElem.find('width').text
+        
+    try:
+        os.remove(destFile)
+    except WindowsError as (errno, strerror):
+        #print "WindowsError({0}): {1}".format(errno, strerror)
+        #print "Note: No "+destFile+" to remove."
+        pass
+    
+    templateTree = ElementTree()
+    templateRoot = templateTree.parse(photoTemplateFile, parser = CommentedTreeBuilder())
+    
+    photoPageElem = findElemWithId(templateRoot, 'div', 'photoDiv')
+    style = 'height:'+str(int(height)+50)+'px; width:'+str(int(width)+50)+'px;'
+    photoPageElem.set('style', style)
+        
+    imgElem = findElemWithId(templateRoot, 'img', 'imageTag')
+    imgElem.set('src', srcFileRoot+'.jpg')
+    imgElem.set('height', height)
+    imgElem.set('width', width)
+    
+    creditSpanElem = findElemWithId(templateRoot, 'span', 'creditSpan')
+    creditHrefElem = creditSpanElem.find('a')
+    photographerTree = ElementTree()
+    photographerRoot = photographerTree.parse(photoElem.find('photographer').text+'.xml')
+    creditHrefElem.text = 'Photo: '+photographerRoot.find('name').text
+    creditHrefElem.set('href', photographerRoot.find('url').text)
+    
+    captionDivElem = findElemWithId(templateRoot, 'div', 'caption')
+    captionElem = photoElem.find('caption')
+    captionDivElem.text = captionElem.text
+    captionDivElem.extend(list(captionElem))
+    captionDivElem.set('style', 'width:'+width+'px;')#    
+    dest = open(destFile, 'w')
+    dest.write(doctype)
+    templateTree.write(dest)
     dest.close()
 
 print "Done!"
