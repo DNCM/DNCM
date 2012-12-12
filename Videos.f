e6f4ef6c995c47248d8b5d8097f2396f3dfa7d71
U-MTP-WS1\Geoff <Geoff@MTP-WS1.(none)>
2012-12-12 07:53:52 -0600

    Added "How to Rotate a Mast" video link.

--- Videos.f
+++ Videos.f
@@ -1,8 +1,12 @@
 <fragment>
 <div style="padding-left:50px; padding-top:10px; margin-bottom:10px;background-color:white;">
-<div style="padding-left:10px;padding-top:5px; border-style:solid; border-width:1px; border-color:black; width:421px;">
-<p style="line-height:1; font-size:12pt; font-weight:bold; margin-bottom:5px;">Watch Bob Rast and Mark Isabel build a mast:</p>
-<a href="http://www.youtube.com/watch?v=HUQ9K4xNrIA" target="_blank" style="margin-bottom:10px;"><img src="YouTubeHUQ9K4xNrIA_413x315.jpg" /></a>
-</div>
+   <div style="padding-left:10px;padding-top:5px; border-style:solid; border-width:1px; border-color:black; width:421px;">
+      <p style="line-height:1; font-size:12pt; font-weight:bold; margin-bottom:5px;">Watch Bob Rast and Mark Isabel Build a Mast:</p>
+      <a href="http://www.youtube.com/watch?v=HUQ9K4xNrIA" target="_blank" style="margin-bottom:10px;"><img src="YouTubeHUQ9K4xNrIA_413x315.jpg" /></a>
+   </div>
+   <div style="margin-top:10px;padding-left:10px;padding-top:5px; border-style:solid; border-width:1px; border-color:black; width:421px;">
+      <p style="line-height:1; font-size:12pt; font-weight:bold; margin-bottom:5px;">How To Eliminate Runs When Coating a Mast:</p>
+      <a href="https://www.facebook.com/photo.php?v=486544164699810" target="_blank" style="margin-bottom:10px;"><img src="HowToRotateAMast_360x360.jpg" /></a>
+   </div>
 </div>
 </fragment>
