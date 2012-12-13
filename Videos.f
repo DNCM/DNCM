b721aad1ee9d706744789073525f480f552e4ca4
U-MTP-WS1\Geoff <Geoff@MTP-WS1.(none)>
2012-12-12 23:17:42 -0600

    Added another video

--- Videos.f
+++ Videos.f
@@ -5,6 +5,10 @@
       <a href="http://www.youtube.com/watch?v=HUQ9K4xNrIA" target="_blank" style="margin-bottom:10px;"><img src="YouTubeHUQ9K4xNrIA_413x315.jpg" /></a>
    </div>
    <div style="margin-top:10px;padding-left:10px;padding-top:5px; border-style:solid; border-width:1px; border-color:black; width:421px;">
+      <p style="line-height:1; font-size:12pt; font-weight:bold; margin-bottom:5px;">Finishing Up a Mast:</p>
+      <a href="http://www.youtube.com/watch?v=0qkpTu6J0j8" target="_blank" style="margin-bottom:10px;"><img src="MastFinishing_413x252.jpg" /></a>
+   </div>
+   <div style="margin-top:10px;padding-left:10px;padding-top:5px; border-style:solid; border-width:1px; border-color:black; width:421px;">
       <p style="line-height:1; font-size:12pt; font-weight:bold; margin-bottom:5px;">How To Eliminate Runs When Coating a Mast:</p>
       <a href="https://www.facebook.com/photo.php?v=486544164699810" target="_blank" style="margin-bottom:10px;"><img src="HowToRotateAMast_360x360.jpg" /></a>
    </div>
